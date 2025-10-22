fa3115d81c1280cfc0248e2221be49b4413bf3da
cqtin2024 <cqtin.2024@gmail.com>
2025-10-23 01:14:47 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "pickleball-cache-v2";
+const ASSETS = [
+  "./",
+  "./score34.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "https://cdn.tailwindcss.com"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+});
